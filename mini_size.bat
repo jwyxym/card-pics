3cc3afa3f1604e0986313052ee98af5db65e3849
jwyxym <jwyxym@126.com>
2025-09-19 00:24:53 +0800

    fix

---
+++ mini_size.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set SOURCE_FOLDER=.\before_pic
+set OUTPUT_FOLDER=.\pics
+set QUALITY=80
+set WIDTH=177
+if not exist "%OUTPUT_FOLDER%" (
+    mkdir "%OUTPUT_FOLDER%"
+)
+
+for %%f in ("%SOURCE_FOLDER%\*.jpg") do (
+    set "filename=%%~nf"
+    set "outfile=%OUTPUT_FOLDER%\%%~nf.jpg"
+
+    echo %%~nxf
+    magick convert "%%f" -resize %WIDTH%x -quality %QUALITY% "!outfile!"
+)
+
+pause
